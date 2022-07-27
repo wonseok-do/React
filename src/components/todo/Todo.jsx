9a58619ca8275c47137c3ec1210f89b4b8b8d9f9
wonseok-do <wsdo970@gmail.com>
2022-07-27 20:20:23 +0900

    Todo jsx분리

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,2 +1,22 @@
 import React from "react";
 import "./style.css";
+
+function Todo(props) {
+  const { todo, Remove, Toggle } = props;
+  return (
+    <div className="todo-card">
+      <h2>{todo.title}</h2>
+      <p>{todo.body}</p>
+      <div className="btn-group">
+        <button className="del-btn" onClick={() => Remove(todo.id)}>
+          삭제하기
+        </button>
+
+        <button className="com-btn" onClick={() => Toggle(todo.id)}>
+          {todo.isDone ? "취소" : "완료"}
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Todo;
