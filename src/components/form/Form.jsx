47f5fcf8235dce54b17fa654e4e9042d36050d76
wonseok-do <wsdo970@gmail.com>
2022-07-27 20:15:40 +0900

    Form jsx분리

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,2 +1,44 @@
 import React from "react";
 import "./style.css";
+
+function Form(props) {
+  const { setTitle, setBody, setTodos, title, body, todos } = props;
+  return (
+    <div className="form">
+      <div className="input-group">
+        <p>제목</p>
+        <input
+          type="text"
+          className="input"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <p>내용</p>
+        <input
+          type="text"
+          className="input"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        />
+      </div>
+      <button
+        className="btn"
+        onClick={() => {
+          setTodos([
+            ...todos,
+            { id: todos.length + 1, title: title, body: body, isDone: false },
+          ]);
+          setTitle("");
+          setBody("");
+        }}
+      >
+        추가하기
+      </button>
+    </div>
+  );
+}
+export default Form;
