7ceb66596b66e52d80030b56d2f171e46219c1c4
wonseok-do <wsdo970@gmail.com>
2022-07-27 20:19:12 +0900

    List jsx분리

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,2 +1,16 @@
 import React from "react";
+import Todo from "../todo/Todo";
 import "./style.css";
+
+function List(props) {
+  const { todo, Remove, Toggle } = props;
+
+  return (
+    <>
+      <div className="todo-container">
+        <Todo Remove={Remove} Toggle={Toggle} todo={todo} />
+      </div>
+    </>
+  );
+}
+export default List;
