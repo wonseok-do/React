925fb65b2bd5fec58c9b20362ee9f80ac767c24b
wonseok-do <wsdo970@gmail.com>
2022-07-27 20:24:57 +0900

    TodoList jsx분리

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,1 +1,32 @@
 import React from "react";
+import List from "../components/list/List";
+import "./style.css";
+
+function TodoList(props) {
+  const { todos, Remove, Toggle } = props;
+  return (
+    <div className="list-container">
+      <h2>Working..🔥</h2>
+      <div className="list-wrapper">
+        {todos.map((todo) => {
+          if (todo.isDone === false) {
+            return <List Remove={Remove} Toggle={Toggle} todo={todo} />;
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <h2>Done..!👍</h2>
+      <div className="list-wrapper">
+        {todos.map((todo) => {
+          if (todo.isDone !== false) {
+            return <List Remove={Remove} Toggle={Toggle} todo={todo} />;
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
+}
+export default TodoList;
